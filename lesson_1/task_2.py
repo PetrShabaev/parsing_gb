0f0b394a52300a7c068b7645116c981762b2a40e
PetrShabaev <shabaevpetr@icloud.com>
2022-09-17 21:06:20 +0300

    complete second task

--- lesson_1/task_2.py
+++ lesson_1/task_2.py
@@ -1,2 +1,19 @@
-TOKEN = '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'
-USERNAME = 'PetrShabaev'
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv("../secret_info")
+
+API_key = os.getenv("API_key")
+city_name = input('Введите название города')
+URL = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_key}'
+
+
+def get_request(url):
+    request = requests.get(url, params={"units": "metric"})
+    if request.status_code == 200:
+        print(request.json())
+
+
+if __name__ == "__main__":
+    get_request(URL)
